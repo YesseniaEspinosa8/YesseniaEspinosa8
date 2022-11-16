bcdac3d3e3128ff0d363f8b24f45fac284017e0d
YesseniaEspinosa8 <117835767+YesseniaEspinosa8@users.noreply.github.com>
2022-11-16 07:25:08 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function tarjeta(numero) {
+    Number = Number.toString()
+    acumulador = ""
+    for (let i = 0; i<Number.length;i++){
+  if(i<4){
+  
+    acumulador = acumulador + Number  [1]
+  } else {
+    acumulador = acumulador + "#"
+  
+  
+  }
+  
+  
+    }
+    return acumulador
+  
+    
+  }
+  tarjeta(4152313843065579)
+  
